ee19cde6ea58743099bd247723e523d33003becc
Lily Eichler <lilyeichler61@gmail.com>
2020-10-21 14:53:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
   for (let i=0; i<25; i++) {
-    if (i == 1) {
+    if (i == 1 || i == 11) {
       array[i] = `I am 1 strange loop.`
     }
-    else {
+    else if (i != 11) {
     array[i] = `I am ${i} strange loops.`
   }
   }
